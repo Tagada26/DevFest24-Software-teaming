080aaf316eab6d6215501bb185034717ee13a5ba
Marjorie <aubertmarjorie2@gmail.com>
2024-10-10 10:19:34 +0200

    Setup repo for devfest hands-on

---
+++ nearest-color.spec.ts
@@ -0,0 +1,16 @@
+import { describe, it, expect } from "vitest";
+
+const guessNearestColor = () => {
+  //Todo
+};
+
+describe("Guess nearest primary colors", () => {
+  it("My first test ", () => {
+    //Given
+    //When
+    const nearestColor = guessNearestColor();
+
+    //Then
+    expect(nearestColor).toBeDefined;
+  });
+});
